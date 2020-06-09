d5cf43f86be5d4085b28868b6f18e805bcfd3f6e
Kov <44253207+kovzhu@users.noreply.github.com>
2020-06-09 20:13:26 +0800

    Update National stats.py

--- National stats.py
+++ National stats.py
@@ -7,7 +7,7 @@ def gettime():
     return int(round(time.time()*1000))
 
 
-def GetJsonData():
+def GetJsonData(DataYears,DataCode):
     url = r'http://data.stats.gov.cn/easyquery.htm'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
     
@@ -18,20 +18,28 @@ def GetJsonData():
     keyvalue['rowcode'] = 'zb'
     keyvalue['colcode'] = 'sj'
     keyvalue['wds'] = '[]'
-    keyvalue['dfwds'] = '[{"wdcode":"zb","valuecode":"A070B"}]'
+    dfws = {}
+    keyvalue['dfwds'] = '[{"wdcode":"zb","valuecode":"' + DataCode +'"}]'
     keyvalue['k1'] = str(gettime())
     keyvalue['h']=1
     # Start a session
     s = requests.session()
-    response = s.get(url,headers=headers, params = keyvalue)
-    # Change the keyvalue for 20 years of data
-    keyvalue['dfwds']='[{"wdcode":"sj","valuecode":"LAST20"}]'
-    # Get the 20 years data
-    response = s.get(url,headers=headers, params = keyvalue)
+    if DataYears == 10:
+
+        response = s.get(url,headers=headers, params = keyvalue)
+        # Change the keyvalue for 20 years of data
+
+    elif DataYears == 20:# Get the 20 years data
+        response = s.get(url,headers=headers, params = keyvalue)
+        keyvalue['dfwds']='[{"wdcode":"sj","valuecode":"LAST20"}]'
+        keyvalue['k1']=str(gettime())
+        response = s.get(url,headers=headers, params = keyvalue)
+    else:
+        print('Error of number of years')
     JsonData = dict(json.loads(response.text))['returndata']
     return JsonData
 
-def ExtratTable(JsonData):
+def ExtratTable(JsonData,DataYears):
     datanodes = JsonData['datanodes']
     wdnodes = JsonData['wdnodes']
     items = wdnodes[0]['nodes']
@@ -48,20 +56,27 @@ def ExtratTable(JsonData):
         data.append(datanodes[i]['data']['strdata'])
         year.append(datanodes[i]['wds'][1]['valuecode'])
         code.append(datanodes[i]['wds'][0]['valuecode'])  
+    # data = [float(i) for i in data]
     table = pd.DataFrame({'Items':itemlist, 'Unit':itemUnit})
-    for i in range(0,20):
+    for i in range(0,DataYears):
         table[str(year[i])]=0    
     for i in range(0,len(itemlist)):
-        table.iloc[i,2:]=data[i*20:(i+1)*20]
+        table.iloc[i,2:]=data[i*DataYears:(i+1)*DataYears]
 
     return table 
 
 def main():
-    JsonData = GetJsonData()
-    Table = ExtratTable(JsonData)
-    Table.to_excel('National Stats.xlsx')
+    DataCode ={'ResourceProd':'A070B',
+                'OilBalance':'A070Q',
+                'GasBalance':'A0710'}
+    DataYears = 10
+    with pd.ExcelWriter('National Stats Data.xlsx') as writer:
+        for i in DataCode:
+            Table = ExtratTable(GetJsonData(DataYears,DataCode[i]),DataYears)
+            Table.to_excel(writer,sheet_name=i)
+        
+    # Table.to_excel('National Stats.xlsx')
+    # print(Table)
 
 if __name__ == '__main__':
     main()    
-    
-
